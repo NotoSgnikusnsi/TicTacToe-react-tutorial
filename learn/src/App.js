8ce5e3f8af76fa20ec870d67ce0971fa3ce1b8d1
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-03-20 13:54:38 +0900

    feat: Create Board

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,4 +1,27 @@
 import React from 'react';
-export default function Square() {
-  return <button className="square">X</button>;
+
+function Square({ value }) {
+  return <button className="square">{value}</button>
+}
+
+export default function Board() {
+  return (
+    <span>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </span>
+  );
 }
